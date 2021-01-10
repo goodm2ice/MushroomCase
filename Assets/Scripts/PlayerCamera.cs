684c1ab74c20ff5c0a1541af749911204604d5e0
goodm2ice <hamel2517@gmail.com>
2021-01-10 19:46:03 +0500

    Скрипт камеры завершён

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -5,35 +5,57 @@ using System;
 
 public class PlayerCamera : MonoBehaviour
 {
-    public float distance = 3;
+    public Transform player;
+
+    public RangedValue distance = new RangedValue(3, 10, 5);
+    public RangedValue horizontal = new RangedValue(-240, 240, -240);
     public float yAngle = 0;
-    public RangedValue hValue = new RangedValue(-240, 240, -240);
+    public Vector4 speed = new Vector4(2, 2, 2, 1);
 
     private Transform hAxis;
-    private Transform yAxis;
+    private Transform camera;
+
+    [SerializeField]
+    private string axisCameraX = "Camera X";
+    [SerializeField]
+    private string axisCameraY = "Camera Y";
+    [SerializeField]
+    private string axisRotateCamera = "Rotate Camera";
+    [SerializeField]
+    private string axisScaleCamera = "Scale Camera";
 
     void Start()
     {
         if (hAxis == null)
-            hAxis = transform.parent;
-        if (yAxis == null)
-            yAxis = hAxis.parent;
+            hAxis = transform.GetChild(0);
+        if (camera == null)
+            camera = hAxis.GetChild(0);
 
         hAxis.localPosition = Vector3.zero;
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetButton("Rotate Camera"))
+        if (Input.GetButton(axisRotateCamera))
         {
-            hValue.cur -= Input.GetAxis("Camera Y");
-            yAngle += Input.GetAxis("Camera X");
+            horizontal.cur -= Input.GetAxis(axisCameraY);
+            yAngle += Input.GetAxis(axisCameraX);
         }
 
-        transform.localPosition = new Vector3(0, distance, 0);
-        transform.LookAt(hAxis);
-        hAxis.localRotation = Quaternion.Euler(hValue.cur / (float)Math.PI, 0, 0);
-        yAxis.localRotation = Quaternion.Euler(0, yAngle / (float)Math.PI, 0);
+        if(Input.GetAxis(axisScaleCamera) != 0) {
+            distance.cur += Input.GetAxis(axisScaleCamera);
+        }
+
+        transform.position = Vector3.Lerp(transform.position, player.position, Time.deltaTime * speed.w);
+
+        Vector3 dD = Vector3.up * distance.cur;
+        camera.localPosition = Vector3.Lerp(camera.localPosition, dD, Time.deltaTime * speed.z);
+        camera.LookAt(player);
+
+        Quaternion dH = Quaternion.AngleAxis(horizontal.cur / (float)Math.PI, Vector3.right);
+        hAxis.localRotation = Quaternion.Slerp(hAxis.localRotation, dH, Time.deltaTime * speed.y);
+        
+        Quaternion dY = Quaternion.AngleAxis(yAngle / (float)Math.PI, Vector3.up);
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, dY, Time.deltaTime * speed.x);
     }
 }
