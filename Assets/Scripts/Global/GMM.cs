f5c0875e2b8b4fffb23710ab05a7a9973f5b966e
goodm2ice <hamel2517@gmail.com>
2021-01-11 01:53:19 +0500

    Добавлен метод преобразования со стандартным значением

---
+++ Assets/Scripts/Global/GMM.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GMM : MonoBehaviour
+{
+    public static T ParseOr<T>(string val, T _default)
+    {
+        try
+        {
+            return (T)System.Convert.ChangeType(val, typeof(T));
+        }
+        catch
+        {
+            return _default;
+        }
+    }
+}
