f40afb30c1aa2a4a6887e5829e58a07a339bb0b5
goodm2ice <hamel2517@gmail.com>
2021-01-10 21:18:11 +0500

    Менджен глобальных констант добавлен

---
+++ Assets/Scripts/Global/GCM.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GCM : MonoBehaviour
+{
+    [System.Serializable]
+    struct Constant
+    {
+        public string name, value;
+    }
+
+    [SerializeField]
+    private List<Constant> _consts;
+
+    private static Dictionary<string, string> consts = new Dictionary<string, string>();
+
+    void Awake()
+    {
+        foreach (Constant c in _consts)
+            consts[c.name] = c.value;
+    }
+
+    public static string Get(string name) {
+        return consts[name] ?? "Undefined";
+    }
+}
