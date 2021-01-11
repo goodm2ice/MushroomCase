9b8812f0d76b78af7b58bb7bf09b16a5b4666d81
goodm2ice <hamel2517@gmail.com>
2021-01-11 05:24:17 +0500

    Добавлены анимации движения и их управление

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,11 +9,22 @@ public class PlayerMove : MonoBehaviour {
     public float forwardSpeed = 2;
     public float backwardSpeed = 0.75f;
     public float rotationSpeed = 100;
+    public Animator playerAnimator;
+
+    private void Start()
+    {
+        playerAnimator = playerAnimator ?? transform.GetComponentInChildren<Animator>();
+    }
 
     void FixedUpdate()
     {
         float rawV = Input.GetAxis("Vertical");
         rawV *= rawV > 0 ? (Input.GetButton("Run") ? runSpeed : forwardSpeed) : backwardSpeed;
+        
+        playerAnimator?.SetBool("walk", rawV != 0);
+        playerAnimator?.SetBool("mirror", rawV < 0);
+        playerAnimator?.SetFloat("speed", rawV != 0 ? Math.Abs(rawV) / 2 : 1);
+        
         Vector3 dP = transform.forward * rawV;
         transform.position += dP * Time.deltaTime;
         transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime);
